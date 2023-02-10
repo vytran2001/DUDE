77db0279d2511609d8f8eead0b356aaf4c8f7e2c
Vy Tran <71632559+vytran2001@users.noreply.github.com>
2023-02-10 13:53:35 +0200

    Add files via upload

---
+++ src/components/functional-components/TableBody.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function TableBody() {
+    return (
+            <tbody>
+                <tr>
+                    <td> Loid Forger</td>
+                    <td> Psychiatrist</td>
+                    <td> Father</td>
+                </tr>
+                <tr>
+                    <td> Anya Forger</td>
+                    <td> Elementary Student</td>
+                    <td> (Adopted) Daughter</td>
+                </tr>
+                <tr>
+                    <td> Yor Forger-Briar</td>
+                    <td> City Hall Clerk</td>
+                    <td> Step-Mother</td>
+                </tr>
+                <tr>
+                    <td> Bond Forger</td>
+                    <td> Pet</td>
+                    <td> Family Dog</td>
+                </tr>
+            </tbody>
+        )
+}
---
+++ src/components/functional-components/TableHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TableHeader() {
+    return (
+            <thead>
+                <th> Name </th>
+                <th> Vocation </th>
+                <th> Family Role </th>
+            </thead>
+    )
+}
